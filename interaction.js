4c760e8d12f336647aadf177c9164ed8f6b4e745
Zoe Ingram <zoeingram@Zoes-MacBook-Pro.local>
2022-11-24 14:06:32 -0800

    checkpoint

--- interaction.js
+++ interaction.js
@@ -1,51 +1,52 @@
 
 function setup() {
-    createCanvas(1200, 1200);
+    createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
 }
 
+let eyex = 100;
+// let keyCode = 0;
 function draw() {
     background(220);
-    // noFill()
+    // fill(keyCode);
     const leftLineX = 200;
     const rightLineX = 800;
     const mirrorStartY = 20;
     const mirrorHeight = 500;
     const mirrorEndY = mirrorStartY + mirrorHeight
 
-    const objx = 300;
-    const objy = mirrorHeight - 50
-    const eyex = 520;
-    const eyey = mirrorHeight + 50;
+    const objx = 400;
+    const objy = mirrorHeight - 100
+    const eyey = mirrorHeight + 200;
 
     let theta = 90 + atan((objx - eyex) / (objy - eyey));
 
     const colisionLineX = eyex < objx ? rightLineX : leftLineX;
     const reflectLineXDist = colisionLineX - objx;
     const reflectLineYDist = reflectLineXDist * tan(theta);
+    const startX = colisionLineX;
+    const startY = objy - reflectLineYDist;
 
     drawMirrors(leftLineX, rightLineX, mirrorStartY, mirrorHeight);
     drawObject(objx, objy);
     drawEye(eyex, eyey);
     line(eyex, eyey, objx, objy);
-    line(objx, objy, colisionLineX, objy - reflectLineYDist);
+    if (startY > mirrorEndY) {
+        return;
+    }
+    // a collision will happen
+    line(objx, objy, startX, startY);
     text(theta, 50, 50);
 
-    const startX = colisionLineX;
-    const startY = objy - reflectLineYDist;
-
-    function drawIntersection(startX, startY, theta, isLeft) {
+    function drawIntersection(startX, startY, theta, isRight) {
         const thetaPrime = 180-theta;
         calculateNumImages(theta);
-
-
-        // text(thetaPrime, 50, 100);
-        const newCLineX = isLeft ? leftLineX : rightLineX;
-        const newYDistance = (!isLeft ? -1 : 1) * (rightLineX-leftLineX) * tan(thetaPrime)
+        const newCLineX = isRight ? leftLineX : rightLineX;
+        const newYDistance = (!isRight ? -1 : 1) * (rightLineX-leftLineX) * tan(thetaPrime)
         const newCLineY = startY + newYDistance;
         line(startX, startY, newCLineX, newCLineY);
-        if (newCLineY < mirrorEndY) {
-            drawIntersection(newCLineX, newCLineY, thetaPrime, !isLeft);
+        if (newCLineY > mirrorStartY) {
+            drawIntersection(newCLineX, newCLineY, thetaPrime, !isRight);
         }
     }
 
@@ -72,17 +73,16 @@ function drawObject(x, y) {
 }
 
 function drawEye(x, y) {
-
     ellipse(x, y, 20, 10)
 }
 
-function mouseDragged(event) {
-    if (event.clientX > 99 && event.clientX < 120) {
-        console.log('left mirror ')
-        //change mirror angle
-    } else if (event.clientX > 200 && event.clientX < 220) {
-        console.log('right mirror ')
-        //change mirror angle
+function keyPressed() {
+    debugger;
+    if (keyCode === LEFT_ARROW) {
+        eyex -= 50;
+        draw();
+    } else if (keyCode === RIGHT_ARROW) {
+        eyex += 50
+        draw();
     }
-
-}
+  }

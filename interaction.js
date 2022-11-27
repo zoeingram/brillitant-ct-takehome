ba89f175f9bf97f2f992039c68253efe4f7ff2b5
Zoe Ingram <zoeingram@Zoes-MacBook-Pro.local>
2022-11-27 11:24:54 -0800

    Adding lesson info

--- interaction.js
+++ interaction.js
@@ -4,11 +4,10 @@ function setup() {
     angleMode(DEGREES);
 }
 
-let eyex = 100;
-// let keyCode = 0;
+let eyex = 500;
+
 function draw() {
-    background(220);
-    // fill(keyCode);
+    background(255);
     const leftLineX = 200;
     const rightLineX = 800;
     const mirrorStartY = 20;
@@ -34,17 +33,27 @@ function draw() {
     if (startY > mirrorEndY) {
         return;
     }
+    
     // a collision will happen
     line(objx, objy, startX, startY);
     text(theta, 50, 50);
-
+    let count = 1;
     function drawIntersection(startX, startY, theta, isRight) {
-        const thetaPrime = 180-theta;
+        const thetaPrime = 180 - theta;
         calculateNumImages(theta);
         const newCLineX = isRight ? leftLineX : rightLineX;
-        const newYDistance = (!isRight ? -1 : 1) * (rightLineX-leftLineX) * tan(thetaPrime)
+        const newYDistance = (!isRight ? -1 : 1) * (rightLineX - leftLineX) * tan(thetaPrime)
         const newCLineY = startY + newYDistance;
         line(startX, startY, newCLineX, newCLineY);
+        count += 1;
+        let newObjX;
+        const distance = count * Math.abs(colisionLineX - objx);
+        if (isRight) {
+            newObjX = distance + rightLineX;
+        } else {
+            newObjX = leftLineX - distance;
+        }
+        drawObject(newObjX, objy);
         if (newCLineY > mirrorStartY) {
             drawIntersection(newCLineX, newCLineY, thetaPrime, !isRight);
         }
@@ -52,28 +61,42 @@ function draw() {
 
 
     if (reflectLineYDist < mirrorEndY) {
-        drawIntersection(startX, startY, theta, eyex < objx);
+        const isRight = eyex < objx;
+
+        let newObjX;
+        if (isRight) {
+            newObjX = Math.abs(colisionLineX - objx) + rightLineX;
+        } else {
+            newObjX = leftLineX - Math.abs(colisionLineX - objx);
+        }
+        drawObject(newObjX, objy);
+        drawIntersection(startX, startY, theta, isRight);
     } else {
         text('no-collision', 100, 200);
     }
 }
 
 function calculateNumImages(theta) {
-    const numImages = (Math.round(360/theta) -1);
+    const numImages = (Math.round(360 / theta) - 1);
     text(numImages, 50, 200);
 }
 
 function drawMirrors(leftLineX, rightLineX, mirrorStartY, mirrorHeight) {
+    fill(200);
     rect(leftLineX, mirrorStartY, 10, mirrorHeight);
     rect(rightLineX, mirrorStartY, 10, mirrorHeight);
 }
 
 function drawObject(x, y) {
+    fill('orange');
     ellipse(x, y, 20, 20)
+    fill(200);
 }
 
 function drawEye(x, y) {
+    fill('blue');
     ellipse(x, y, 20, 10)
+    fill(200);
 }
 
 function keyPressed() {
@@ -85,4 +108,4 @@ function keyPressed() {
         eyex += 50
         draw();
     }
-  }
+}

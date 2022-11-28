b305b4c475397eeab950d5c55ab8bc3f46e1823b
Zoe Ingram <zoeingram@Zoes-MacBook-Pro.local>
2022-11-27 17:08:13 -0800

    Adding a ton of comments

--- interaction.js
+++ interaction.js
@@ -1,84 +1,148 @@
 
 function setup() {
-    createCanvas(windowWidth, windowHeight + 100);
     angleMode(DEGREES);
+    background(255);
 }
 
-let eyeX = 800;
+let eyeAdjustment = 0;
 const MIRROR_WIDTH = 10;
 
+const MIRROR_SEPARATION_DISTANCE = 200;
+
 function draw() {
-    background(255);
-    const leftLineX = 710;
-    const rightLineX = 900;
-    const mirrorXDistance = rightLineX - leftLineX;
+    //
+    // GENERAL SETUP
+    //
+    createCanvas(windowWidth, windowHeight + 100);
+    // Set the center point of the window
+    const centerPoint = windowWidth / 2;
+
+    const distanceFromMidpoint = MIRROR_SEPARATION_DISTANCE / 2;
+    // Set the positions of the mirror
+    // Have to adjust for the MIRROR_WIDTH on the left side, so we add that
+    const leftLineX = (centerPoint - distanceFromMidpoint) + MIRROR_WIDTH;
+    const rightLineX = centerPoint + distanceFromMidpoint;
+
     const mirrorStartY = 20;
     const mirrorHeight = 500;
-    const mirrorEndY = mirrorStartY + mirrorHeight
+    const mirrorEndY = mirrorStartY + mirrorHeight;
+    // Take the width back off the x coordinate
+    drawMirrors(leftLineX - MIRROR_WIDTH, rightLineX, mirrorStartY, mirrorHeight);
 
-    const objX = 800;
+    // Set the position of the reflected object
+    const objX = centerPoint;
     const objY = mirrorHeight - 10
+
+    drawObject(objX, objY);
+
+    // Set the position of the eye
+    const eyeX = objX + eyeAdjustment;
     const eyeY = mirrorHeight + 50;
 
+    drawEye(eyeX, eyeY);
+
+    // We have our coordinates, now we can draw the line
+    line(eyeX, eyeY, objX, objY);
+
+    //
+    // INITIAL MATH + FIRST COLLISION
+    //
+
+    // We want to calculate theta based on the angle of intersection of the eye and the object
+    // Since we have the x + y coordinates of both the eye and the object, it's easy to think of this as a right triangle, where we know the length of two sides.
+    // In order to calculate the angle of theta, we can use arctangent to calculate the angle (see https://www.mathsisfun.com/algebra/trig-finding-angle-right-triangle.html) for some trig references.
+    // We then add 90 degrees to correct the angle to the correct location
     let theta = 90 + atan((objX - eyeX) / (objY - eyeY));
 
-    const colisionLineX = eyeX < objX ? rightLineX : leftLineX;
-    const reflectLineXDist = colisionLineX - objX;
-    const reflectLineYDist = reflectLineXDist * tan(theta);
-    const startX = colisionLineX;
-    const startY = objY - reflectLineYDist;
+    // A variable to track which side the line would be colliding with
+    const collisionOnRightSide = eyeX < objX;
+    const colisionLineX = collisionOnRightSide ? rightLineX : leftLineX;
 
-    drawMirrors(leftLineX - MIRROR_WIDTH, rightLineX, mirrorStartY, mirrorHeight);
-    drawObject(objX, objY);
-    drawEye(eyeX, eyeY);
-    line(eyeX, eyeY, objX, objY);
-    if (startY > mirrorEndY) {
+    // Calculate the X component of the distance of the collision line
+    const collisionLineXDist = colisionLineX - objX;
+    // Opposite (Y) = Adjacent (X) * tan(theta)
+    const reflectLineYDist = collisionLineXDist * tan(theta);
+    // Set out coordinates
+    const collisionLineY = objY - reflectLineYDist;
+
+    if (collisionLineY > mirrorEndY) {
+        // If we are in this conditional, then there is no colision of the line above and either of the mirrors. We can exit early
         return;
     }
 
-    // a collision will happen
-    line(objX, objY, startX, startY);
+    // If we have gotten this far, then we know a collision will happen.
+    // Draw the line to the colliding mirror
+    line(objX, objY, colisionLineX, collisionLineY);
+    
+    // Write theta to the screen
     fill(0);
     textSize(20);
-
-    text("θ = " + Math.round(theta), windowWidth - 200, 50);
+    // Our calculations are technically not using exactly theta; they are using the converse of theta.
+    // The math works either way, but for display, subtract 90.
+    text("θ = " + Math.round(Math.abs(theta-90)), windowWidth - 200, 50);
     fill(235);
 
-    let count = 0;
-    function drawIntersection(startX, startY, theta, isRight) {
-        const thetaPrime = 180 - theta;
-        const newCLineX = isRight ? leftLineX : rightLineX;
-        const newYDistance = (!isRight ? -1 : 1) * (rightLineX - leftLineX) * tan(thetaPrime)
-        const newCLineY = startY + newYDistance;
-        line(startX, startY, newCLineX, newCLineY);
-        let newObjX;
-        const distance = count * mirrorXDistance + Math.abs(colisionLineX - objX);
-        if (isRight) {
-            newObjX = distance + rightLineX;
+    /**
+     * This function draws the remaining reflection collisions as well as the reflected objects
+     * It operates recursively, so based on the initial theta (and the heights of the mirrors), it will draw all remaining reflections.
+     * 
+     * @param {*} startY the starting y position
+     * @param {*} theta the current theta
+     * @param {*} collisionOnTheRight a boolean of whether we are currently colliding on the right mirror. if false, that means we are colliding on the left side
+     * @param {*} depth the depth of the recursion
+     * @returns 
+     */
+    function drawReflection(startY, theta, collisionOnTheRight, depth = 0) {
+        // Since we know we are on one of the mirrors, we can easily derive our starting and end x positions
+        const startX = collisionOnTheRight ? rightLineX : leftLineX;
+        const endX = collisionOnTheRight ? leftLineX : rightLineX;
+        
+        // The new theta value will be 180 minus theta because of the reflection.
+        const reflectedTheta = 180 - theta;
+        // Opposite (Y) = Adjacent (X) * tan(theta)
+        // The positive vs. negative is just based on whether we are going right or left
+        const newYDistance = (!collisionOnTheRight ? -1 : 1) * MIRROR_SEPARATION_DISTANCE * tan(reflectedTheta)
+        const endY = startY + newYDistance;
+
+        // Draw the new reflected line
+        line(startX, startY, endX, endY);
+
+        //
+        // DRAWING THE REFLECTED OBJECT
+        //
+        let reflectedObjectX;
+        // We could do more trig here since we have theta + the height, but we also know that the reflected object distance is a function of the number of reflections
+        // 1. The first reflection will be the equal to the initial distance of the object to the reflecting surface
+        // 2. The second reflection will be the distance of 1 + the distance in between the mirrors etc.
+        const distance = depth * MIRROR_SEPARATION_DISTANCE + Math.abs(collisionLineXDist);
+        if (collisionOnTheRight) {
+            reflectedObjectX = distance + rightLineX;
         } else {
-            newObjX = leftLineX - distance;
+            reflectedObjectX = leftLineX - distance;
         }
-        if (count > 0) {
-            drawMirrors(leftLineX - count*mirrorXDistance, rightLineX + count*mirrorXDistance, mirrorStartY, mirrorHeight);
+        // Draw the virtual mirrors ad-hoc
+        if (depth > 0) {
+            drawMirrors(leftLineX - depth*MIRROR_SEPARATION_DISTANCE, rightLineX + depth*MIRROR_SEPARATION_DISTANCE, mirrorStartY, mirrorHeight);
         }
-        drawObject(newObjX, objY);
+
+        // Draw the reflected object
+        drawObject(reflectedObjectX, objY);
+
+        // Draw a reflected line from the collision point to the reflected object
         setLineDash([5, 5]);
-        line(startX, startY, newObjX, objY);
+        line(startX, startY, reflectedObjectX, objY);
         setLineDash([]);
-        if (newCLineY < mirrorStartY) {
+
+        // If the next set of lines is going to be beyond the mirror, we can stop the recursion
+        if (endY < mirrorStartY) {
             return;
         }
-        count += 1;
-        drawIntersection(newCLineX, newCLineY, thetaPrime, !isRight);
+        // Recurse again on the other side
+        drawReflection(endY, reflectedTheta, !collisionOnTheRight, depth + 1);
     }
 
-
-    if (reflectLineYDist < mirrorEndY) {
-        const isRight = eyeX < objX;
-        drawIntersection(startX, startY, theta, isRight);
-    } else {
-        text('no-collision', 100, 200);
-    }
+    // Start the recursion
+    drawReflection(collisionLineY, theta, collisionOnRightSide);
 }
 
 
@@ -105,10 +169,10 @@ function setLineDash(list) {
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-        eyeX -= 15;
+        eyeAdjustment -= 15;
         draw();
     } else if (keyCode === RIGHT_ARROW) {
-        eyeX += 15
+        eyeAdjustment += 15
         draw();
     }
 }
